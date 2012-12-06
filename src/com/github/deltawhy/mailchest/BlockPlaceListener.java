faab67e31868ac6768a75f4b87ca626ee2453959
Michael Limiero <mike5713@gmail.com>
2012-12-05 19:34:43 -0500

    Attach sign to chest

--- src/com/github/deltawhy/mailchest/BlockPlaceListener.java
+++ src/com/github/deltawhy/mailchest/BlockPlaceListener.java
@@ -1,5 +1,6 @@
 package com.github.deltawhy.mailchest;
 
+import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.block.*;
 import org.bukkit.entity.Player;
@@ -28,6 +29,58 @@ public class BlockPlaceListener implements Listener {
 					&& plugin.getConfig().getBoolean("auto-create.wall")) {
 				plugin.autoCreateMailbox(block, player);
 			}
+		}
+	}
+	
+	@EventHandler
+	public void onSignChange(SignChangeEvent event) {
+		if (event.getLine(0).equals("[" + plugin.getConfig().getString("sign-text") + "]")) {
+			Player player = event.getPlayer();
+			Block signBlock = event.getBlock();
+			BlockFace[] directions = {BlockFace.NORTH, BlockFace.SOUTH, BlockFace.EAST, BlockFace.WEST};
+			Block chest = null;
+			
+			for (BlockFace direction : directions) {
+				if (signBlock.getRelative(direction).getType() == Material.CHEST) {
+					chest = signBlock.getRelative(direction);
+					break;
+				}
+			}
+			
+			if (chest == null) {
+				player.sendMessage(ChatColor.RED + "[MailChest] No chest found.");
+				event.setCancelled(true);
+				return;
+			}
+			
+			//attach sign to chest
+			if (signBlock.getType() == Material.SIGN_POST) {
+				signBlock.setType(Material.WALL_SIGN);
+				switch (signBlock.getFace(chest)) {
+				case NORTH:
+					signBlock.setData((byte) 5);
+					break;
+				case SOUTH:
+					signBlock.setData((byte) 4);
+					break;
+				case EAST:
+					signBlock.setData((byte) 3);
+					break;
+				case WEST:
+					signBlock.setData((byte) 2);
+					break;
+				default:
+					break;
+				}
+				Sign sign = (Sign)signBlock.getState();
+				sign.setLine(0, event.getLine(0));
+				sign.setLine(1, event.getLine(1));
+				sign.setLine(2, event.getLine(2));
+				sign.setLine(3, event.getLine(3));
+				sign.update(true);
+			}
+			
+			player.sendMessage(ChatColor.GOLD + "[MailChest] Created new mailbox");
 		}
 	}
 }
